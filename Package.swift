f6f36dbe0ba73a1bc79aad47fc0aee25d9a9b0d1
huyvuq <vuh@go.stockton.edu>
2024-04-12 11:45:29 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WaveAnimationView",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "WaveAnimationView", targets: ["WaveAnimationView"]),
     ],
